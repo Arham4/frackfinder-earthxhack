6e7349b3afbf767658e48729b7f687a0f6a957c7
user <typhoonrg@gmail.com>
2019-04-27 17:53:48 -0500

    Start machine learning algo.

---
+++ site_scorer/ml_model.py
@@ -0,0 +1,12 @@
+from keras.datasets import mnist
+from autokeras.image.image_supervised import ImageClassifier
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(x_train.shape + (1,))
+x_test = x_test.reshape(x_test.shape + (1,))
+
+clf = ImageClassifier(verbose=True)
+clf.fit(x_train, y_train, time_limit=12 * 60 * 60)
+clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
+y = clf.evaluate(x_test, y_test)
+print(y)
