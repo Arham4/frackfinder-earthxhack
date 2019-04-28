b9241b3be397689911316e21bb83e75b3d96d7a4
user <typhoonrg@gmail.com>
2019-04-27 21:02:48 -0500

    Update ML model and data processing.

--- site_scorer/ml_model.py
+++ site_scorer/ml_model.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 from autokeras.image.image_supervised import ImageClassifier
 
-num_images = 43
+num_images = 47
 
 x_train = []
 for i in range (1,num_images+1):
@@ -12,7 +12,7 @@ for i in range (1,num_images+1):
 print(len(x_train))
 x_train = np.array(x_train)
 
-y_train = [i for i in range (1,num_images+1)] 
+y_train = [5,5,5,4,7,8,6,6,8,10,9,7,7,8,8,9,6,9,4,4,9,3,8,5,1,3,2,6,7,4,7,6,6,2,3,8,3,7,8,4,6,9,5,10,1,0,10]
 
 x_test = []
 img = cv2.imread("images/" + str(num_images) + ".jpg")
@@ -27,7 +27,9 @@ x_train = x_train.reshape(x_train.shape + (1,))
 x_test = x_test.reshape(x_test.shape + (1,))
 
 clf = ImageClassifier(verbose=True)
-clf.fit(x_train, y_train, time_limit=2 * 60)
-clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
-y = clf.evaluate(x_test, y_test)
-print(y)
+clf.fit(x_train, y_train, time_limit=2*60)
+
+y_pred = clf.predict(x_test)
+#clf.final_fit(x_train, y_train, x_test, y_test, retrain=False)
+#y = clf.evaluate(x_test, y_test)
+#print(y)
